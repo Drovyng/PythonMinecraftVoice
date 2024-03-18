daa549ba49255459ecd30f8f2c1ef36778fb968f
Drovyng <TrueDrovyng@gmail.com>
2024-03-18 19:30:37 +0300

    Update install_libraries.bat

--- install_libraries.bat
+++ install_libraries.bat
@@ -1,1 +1,6 @@
+net.exe session 1>NUL 2>NUL || (
+echo This script requires elevated rights. 
+powershell -Command "Start-Process ./generate_exe.bat -Verb RunAs"
+exit
+)
 pip install vosk pyaudio keyboard mouse fuzzywuzzy pyinstaller
